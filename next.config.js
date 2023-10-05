b95356a2e2c8f0f67deae09ed5c6d3989245ac23
Himanshu <himanshushahi0478@gmail.com>
2023-10-05 15:15:59 +0530

    some fix in config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "encrypted-tbn0.gstatic.com",
-            port: "",
-            pathname: "images",
-          },
-        ]
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
